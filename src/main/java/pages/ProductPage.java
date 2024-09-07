d19332415f9621afbb1aed1acf767bca7d44a706
akshit raj patel <32068317+akshitrajpatel@users.noreply.github.com>
2024-09-07 11:13:45 +0530

    Update ProductPage.java

--- src/main/java/pages/ProductPage.java
+++ src/main/java/pages/ProductPage.java
@@ -1,26 +1,42 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class ProductPage {
     WebDriver driver;
+    WebDriverWait wait;
 
-    // Locators for elements on the ProductPage
-    By firstProduct = By.cssSelector(".product-tuple-listing .dp-widget-link");
-    By addToCartButton = By.cssSelector("#add-cart-button-id");
+    // Locators using XPath for elements on the ProductPage
+    By firstProduct = By.xpath("//div[@class='product-tuple-listing']//a[@class='dp-widget-link']");
+    By addToCartButton = By.xpath("//div[@class='btn-segment clearfix']//button[contains(@id, 'add-cart-button-id')]");
+
     // Constructor
     public ProductPage(WebDriver driver) {
         this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Explicit wait for 10 seconds
     }
 
     // Method to select the first product in the list
     public void selectFirstProduct() {
-        driver.findElement(firstProduct).click();
+        wait.until(ExpectedConditions.elementToBeClickable(firstProduct)).click();
     }
 
-    // Method to add the product to the cart
+    // Standard Method to add the product to the cart
     public void addToCart() {
-        driver.findElement(addToCartButton).click();
+        WebElement addToCart = wait.until(ExpectedConditions.elementToBeClickable(addToCartButton));
+        addToCart.click();
+    }
+
+    // Method to add the product to the cart using JavaScriptExecutor
+    public void addToCartUsingJS() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement addToCartButtonElement = driver.findElement(addToCartButton);
+        js.executeScript("arguments[0].click();", addToCartButtonElement);
     }
 }
